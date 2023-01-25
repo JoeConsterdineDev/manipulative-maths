77f098b008fc3f0da3065e5729b73c83a3601488
Joe Consterdine <joe.consterdine@cloudsensesolutions.com>
2023-01-25 19:12:42 +0000

    Added header component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView } from 'react-native';
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-      <Image
-        style={{
-          height: 50,
-          width: 50
-        }}
-        source={require('./assets/logo.svg')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Header />
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    background: 'linear-gradient(to right, #ffa751, #ffe259)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */    
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#002e63',
+    paddingTop: 32
   },
 });
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,31 @@
-import { Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import Logo from '../assets/logo.svg';
 
 const Header = () => {
 	return (
-		<Text>Manipulative Maths</Text>
+		<View style={headerStyles.container}>
+			<Logo />
+			<Text style={headerStyles.title}>Manipulative Maths</Text>
+		</View>
 	)
 }
+
+const headerStyles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		borderBottomColor: '#f5f5f5',
+		borderBottomWidth: 1,
+		backgroundColor: '#fff',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 8,
+	},
+	title: {
+		color: '#002e63',
+		fontSize: 30,
+		fontWeight: 'bold'
+	}
+ });
 
 export default Header;
