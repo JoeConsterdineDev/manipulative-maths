a460876f2de1fcc8b47c39588a0015e90d267a66
Joe Consterdine <joe.consterdine@cloudsensesolutions.com>
2023-01-25 18:12:16 +0000

    Add starter files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Image
+        style={{
+          height: 50,
+          width: 50
+        }}
+        source={require('./assets/logo.svg')}
+      />
     </View>
   );
 }
@@ -13,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    background: 'linear-gradient(to right, #ffa751, #ffe259)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */    
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+const Header = () => {
+	return (
+		<Text>Manipulative Maths</Text>
+	)
+}
+
+export default Header;
